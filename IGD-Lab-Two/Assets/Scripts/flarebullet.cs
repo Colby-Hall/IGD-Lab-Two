4e0d17ffc38ef0458f61c91be42222ab3b1f62aa
Colby-Hall <hallcc@hendrix.edu>
2019-02-19 22:03:16 -0600

    Reorg

--- IGD-Lab-Two/Assets/Scripts/flarebullet.cs
+++ IGD-Lab-Two/Assets/Scripts/flarebullet.cs
@@ -11,11 +11,9 @@ public class flarebullet : MonoBehaviour {
 	public 	float flareTimer = 9;
 	public AudioClip flareBurningSound;
 
-    public GameObject tree;
     public GameObject deadTree;
 
-
-	// Use this for initialization
+   
 	void Start () {
 		
 		GetComponent<AudioSource>().PlayOneShot(flareBurningSound);
@@ -29,36 +27,27 @@ public class flarebullet : MonoBehaviour {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-
-		
-		
-		
 			flarelight.intensity = Random.Range(2f,6.0f);
-			
-		
-		
 			flarelight.intensity =  Mathf.Lerp(flarelight.intensity,0f,Time.deltaTime * smooth);
 			flarelight.range =  Mathf.Lerp(flarelight.range,0f,Time.deltaTime * smooth);			
 			flaresound.volume = Mathf.Lerp(flaresound.volume,0f,Time.deltaTime * smooth);
-        smokepParSystem.maxParticleSize = Mathf.Lerp(smokepParSystem.maxParticleSize, 0f, Time.deltaTime * 5);
+            smokepParSystem.maxParticleSize = Mathf.Lerp(smokepParSystem.maxParticleSize, 0f, Time.deltaTime * 5);
 
-			
 	}
 
-    private void BurnTree(GameObject other)
+    private void BurnTree(Collider other)
     {
         Vector3 treePos = other.transform.position;
         Destroy(other.gameObject);
         Instantiate(deadTree, treePos, Quaternion.identity);
     }
 
-    private void OnParticleCollision(GameObject other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Tree"))
         {
-            BurnTree(other.gameObject);
+            BurnTree(other);
         }
     }
 
--- IGD-Lab-Two/Assets/Scripts/flaregun.cs
+++ IGD-Lab-Two/Assets/Scripts/flaregun.cs
@@ -1,72 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class flaregun : MonoBehaviour {
-	
-	public Rigidbody flareBullet;
-	public Transform barrelEnd;
-	public GameObject muzzleParticles;
-	public AudioClip flareShotSound;
-	public AudioClip noAmmoSound;	
-	public AudioClip reloadSound;	
-	public int bulletSpeed = 10000;
+public class flaregun : MonoBehaviour
+{
+
+    public Rigidbody flareBullet;
+    public Transform barrelEnd;
+    public GameObject muzzleParticles;
+    public AudioClip flareShotSound;
+    public AudioClip noAmmoSound;
+    public AudioClip reloadSound;
+    public float bulletSpeed = 1000;
     public int currentRound = 1;
-	
-	
+
+    public GameObject deadTree;
+    public Camera playerCam;
+
+    private int range = 20;
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-		if(Input.GetButtonDown("Fire1") && !GetComponent<Animation>().isPlaying)
-		{
-			if(currentRound > 0){
-				Shoot();
+    void Start()
+    {
+    }
+
+  
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1") && !GetComponent<Animation>().isPlaying)
+        {
+            if (currentRound > 0)
+            {
+                Shoot();
                 currentRound -= 1;
-			}else{
-				GetComponent<Animation>().Play("noAmmo");
-				GetComponent<AudioSource>().PlayOneShot(noAmmoSound);
-			}
-		}
-		if(Input.GetKeyDown(KeyCode.R) && !GetComponent<Animation>().isPlaying)
-		{
-			Reload();
-			
-		}
-	
-	}
-	
-	void Shoot()
-	{
-			GetComponent<Animation>().CrossFade("Shoot");
-			GetComponent<AudioSource>().PlayOneShot(flareShotSound);
-		
-			
-			Rigidbody bulletInstance;			
-			bulletInstance = Instantiate(flareBullet,barrelEnd.position,barrelEnd.rotation) as Rigidbody; //INSTANTIATING THE FLARE PROJECTILE
-			
-			
-			bulletInstance.AddForce(barrelEnd.forward * bulletSpeed); //ADDING FORWARD FORCE TO THE FLARE PROJECTILE
-			
-			Instantiate(muzzleParticles, barrelEnd.position,barrelEnd.rotation);	//INSTANTIATING THE GUN'S MUZZLE SPARKS	
-           
-		
-	}
-	
-	void Reload()
-	{
+            }
+            else
+            {
+                GetComponent<Animation>().Play("noAmmo");
+                GetComponent<AudioSource>().PlayOneShot(noAmmoSound);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R) && !GetComponent<Animation>().isPlaying)
+        {
+            Reload();
+
+        }
+
+    }
+
+    void Shoot()
+    {
+        GetComponent<Animation>().CrossFade("Shoot");
+        GetComponent<AudioSource>().PlayOneShot(flareShotSound);
+
+
+        Rigidbody bulletInstance;
+        bulletInstance = Instantiate(flareBullet, barrelEnd.position, barrelEnd.rotation) as Rigidbody; //INSTANTIATING THE FLARE PROJECTILE
+
+
+        bulletInstance.AddForce(barrelEnd.forward * bulletSpeed); //ADDING FORWARD FORCE TO THE FLARE PROJECTILE
+
+        Instantiate(muzzleParticles, barrelEnd.position, barrelEnd.rotation);   //INSTANTIATING THE GUN'S MUZZLE SPARKS
+    }
+
+    void Reload()
+    {
         if (currentRound < 1)
         {
             GetComponent<AudioSource>().PlayOneShot(reloadSound);
             GetComponent<Animation>().CrossFade("Reload");
             currentRound += 1;
         }
-		
-	}
+
+    }
 }
